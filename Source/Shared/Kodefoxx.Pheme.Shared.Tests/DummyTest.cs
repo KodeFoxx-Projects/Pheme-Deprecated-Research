461ed85d588ddc0fb5f088305d680423cc28242c
Yves Schelpe <yves.schelpe@gmail.com>
2018-09-28 12:36:23 +0200

    Project structure

---
+++ Source/Shared/Kodefoxx.Pheme.Shared.Tests/DummyTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Kodefoxx.Pheme.Shared.Tests
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void Is_always_true() 
+            => Assert.True(true)
+        ;
+    }
+}
